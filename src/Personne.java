90c9e673e57e0ac5141a628a43d23838a9f34c17
Alexandre Javanshir <alexandre.javanshir@gmail.com>
2024-11-29 14:51:03 +0100

    test1

---
+++ src/Personne.java
@@ -0,0 +1,17 @@
+public class Personne {
+    private int identifiant;
+    private String nom;
+    private String adresse;
+    private int contact;
+
+    public Personne(int identifiant,String nom,String adresse, int contact){
+        this.identifiant = identifiant;
+        this.nom = nom;
+        this.adresse = adresse;
+        this.contact = contact;
+    }
+    public String obtenirInfos(int identifiant,String nom,String adresse, int contact){
+
+    }
+
+}
