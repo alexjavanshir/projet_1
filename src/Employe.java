bc3f8cf9fdaf944a230da22df5ad7a6ef976fa96
Alexandre Javanshir <alexandre.javanshir@gmail.com>
2024-11-29 15:28:53 +0100

    test1

--- src/Employe.java
+++ src/Employe.java
@@ -1,2 +1,5 @@
 public class Employe extends Personne{
+    public Employe(String nom, String adresse, int contact) {
+        super(nom, adresse, contact);
+    }
 }
--- src/Personne.java
+++ src/Personne.java
@@ -1,17 +1,55 @@
+import java.util.Random;
+
 public class Personne {
     private int identifiant;
     private String nom;
     private String adresse;
     private int contact;
 
-    public Personne(int identifiant,String nom,String adresse, int contact){
-        this.identifiant = identifiant;
+    public Personne(String nom, String adresse, int contact) {
+        this.identifiant = genererIdentifiantAleatoire();
         this.nom = nom;
         this.adresse = adresse;
         this.contact = contact;
     }
-    public String obtenirInfos(int identifiant,String nom,String adresse, int contact){
 
+    public String obtenirInfos() {
+        return String.format("ID : %d, Nom : %s, Adresse : %s, Contact : %d", identifiant, nom, adresse, contact);
     }
 
+    public void setIdentifiant() {
+        this.identifiant = genererIdentifiant();
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public void setContact(int contact) {
+        this.contact = contact;
+    }
+    public int getIdentifiant() {
+        return identifiant;
+    }
+
+    public String getNom(String nom) {
+        return nom;
+    }
+
+    public String getAdresse(String adresse) {
+        return adresse;
+    }
+
+    public int getContact(int contact) {
+        return contact;
+    }
+
+    private int genererIdentifiant() {
+        Random random = new Random();
+        return 100000 + random.nextInt(900000);
+    }
 }
