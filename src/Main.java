3bfa736cb9da83f55b6a1bdee04a63287ddd6cc7
Alexandre Javanshir <alexandre.javanshir@gmail.com>
2024-11-29 16:45:49 +0100

    test1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Vol vol1 = new Vol();
+        vol1.setNumeroVol("AF123");
+        vol1.setOrigine("Paris");
+        vol1.setDestination("New York");
+
+        Avion avion1 = new Avion();
+        avion1.affecterVol(vol1);
+
+        Pilote pilote1 = new Pilote();
+        pilote1.affecterVol(vol1);
+
+        PersonnelCabine cabine1 = new PersonnelCabine();
+        cabine1.affecterVol(vol1);
+
+/*
+        GestionnaireVol gestionnaire = new GestionnaireVol(vol1);
+        gestionnaire.affecterAvion(avion1);
+        gestionnaire.affecterPilote(pilote1);
+        gestionnaire.affecterPersonnelCabine(cabine1);*/
     }
 }
